d27c961aa09a8df75349eb76c338d6f92e1a1b7b
rakshith_s <rakshithsantosh@gmail.com>
2023-12-04 12:39:06 +0530

    background Changer

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color,setColor] = useState("aqua")
+
+  return (
+    <>
+    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className="flex flex-wrap justify-center gap-3 bg-white shadow-lg px-3 py-2 rounded">
+          <button onClick={()=>setColor("red")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+          <button onClick={()=>setColor("aqua")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"aqua"}}>Aqua</button>
+          <button onClick={()=>setColor("green")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+          <button onClick={()=>setColor("violet")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"violet"}}>Violet</button>
+          <button onClick={()=>setColor("grey")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"grey"}}>Grey</button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
